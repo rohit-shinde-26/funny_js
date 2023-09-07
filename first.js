0c1987ee4a1bc675add5cea044296b397fbaf821
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2023-09-07 21:37:21 +0530

    Update first.js
    output added as comment in code

--- first.js
+++ first.js
@@ -1,8 +1,14 @@
 console.log(3 + 3);
+//6
 console.log("3" + "3");
+//33
 console.log("3" + + 3);
+//33
 console.log(3 + + "3");
+//6
 console.log(3 + 3 - 3);
+//3
 console.log("3" + "3" - "3");
+//30
 
 
